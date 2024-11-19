caf0f2aa3d126d0dcaf7e380a7f52609f8389f55
unknown <https://github.com/MuminovMuslimbek>
2024-11-19 17:53:05 +0300

    changed img done

--- js/app.js
+++ js/app.js
@@ -9,11 +9,6 @@ btnRules && btnRules.addEventListener('click', function() {
     modalBg.style.display = 'flex'
 })
 
-modalBg && modalBg.addEventListener('click', function() {
-    modal.style.display = 'none'
-    modalBg.style.display = 'none'
-})
-
 deleteModal.forEach((button) => {
     button.addEventListener('click', function() {
         modal.style.display = 'none'
@@ -21,12 +16,39 @@ deleteModal.forEach((button) => {
     })
 });
 
+modalBg && modalBg.addEventListener('dblclick', function() {
+    modal.style.display = 'none'
+    modalBg.style.display = 'none'
+})
+
+
 // !Count:
 const count = document.querySelector('.count')
 const btn = document.querySelectorAll('.btn')
+const play = document.querySelector('.play')
+const play_res = document.querySelector('.play_res')
+const again = document.querySelectorAll('.again')
+const btnResult = document.querySelectorAll('.btnResult')
+const resImg = document.querySelector('.resImg')
+const btnImg = document.querySelectorAll('.btnImg')
 
-btn.forEach((btn) => {
-    btn.addEventListener('click', function() {
-        count.innerHTML++
+
+function getAtributForImage(index) {
+    return btnImg[index].getAttribute('src')
+}
+
+btn.forEach((button, index) => {
+    button.addEventListener('mouseup', function() {
+        play.style.display = 'none'
+        play_res.style.display = 'flex'
+        const setImg = getAtributForImage(index)
+        resImg.setAttribute('src', setImg);
+    })
+})
+
+again.forEach((againBtn) => {
+    againBtn && againBtn.addEventListener('click', function() {
+        play.style.display = 'flex'
+        play_res.style.display = 'none'
     })
 })
